270e0334a9729b7f74bf09a2801ae538befbd984
cmr <99552538@qq.com>
2020-12-26 17:08:27 +0800

    12-26

--- src/DP/test494.java
+++ src/DP/test494.java
@@ -14,16 +14,27 @@ class Solution494 {
         if (Math.abs(S) > Math.abs(sum)) {
             return 0;
         }
+        //因为有负值所以，列数加上负数
         int[][] dp = new int[nums.length][sum * 2 + 1];
+        //0+num[0]和0-num[0]都有一种路径
         dp[0][sum + nums[0]] = dp[0][sum - nums[0]] = 1;
         if (nums[0] == 0) {
+            //如果num[0]=0，+0-0都可达，所以两种
             dp[0][sum] = 2;
         }
         for (int i = 1; i < nums.length; i++) {
             for (int j = 0; j < sum * 2 + 1; j++) {
-                int l = j - nums[i] < 0 ? 0 : j - nums[i];
-                int r = j + nums[i] > sum * 2 ? 0 : j + nums[i];
-                dp[i][j] = dp[i - 1][l] + dp[i - 1][r];
+                //l和r代表dp[i - 1][j - nums[i]]和dp[i - 1][j + nums[i]]
+                //如果有越界情况则说明不能从j - nums[i]到达j，这条路不可达，路线数量为0
+                int l = 0;
+                int r = 0;
+                if (j - nums[i] >= 0) {
+                    l = dp[i - 1][j - nums[i]];
+                }
+                if (j + nums[i] <= sum * 2) {
+                    r = dp[i - 1][j + nums[i]];
+                }
+                dp[i][j] = l + r;
             }
         }
         return dp[nums.length - 1][sum + S];
